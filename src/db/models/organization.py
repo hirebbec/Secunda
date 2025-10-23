6e097982ed8b2b58661167111a6a0cfb922cdf44
Ruzek <reek867@gmail.com>
2025-10-23 13:45:58 +0300

    Fix organization model

--- src/db/models/organization.py
+++ src/db/models/organization.py
@@ -10,6 +10,6 @@ class Organization(BaseModel, IDMixin, CreatedAtMixin, UpdatedAtMixin):
     __tablename__ = "organisations"
 
     name: Mapped[str] = mapped_column(String, nullable=False)
-    phone_number: Mapped[str] = mapped_column(String, nullable=False)
+    phone_number: Mapped[list[str]] = mapped_column(ARRAY(String), nullable=False)
     building: Mapped[Building] = mapped_column(Integer, ForeignKey("buildings.id"), nullable=False)
     activities: Mapped[list[int]] = mapped_column(ARRAY(Integer), nullable=False)
